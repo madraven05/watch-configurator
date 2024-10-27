be7c2ac407eb946c7d55d16ccb124b9b52141d88
pranshu-kumar <pranshukumargond05@gmail.com>
2024-10-27 22:05:07 +0530

    removed orbit controls

--- src/components/ConfiguratorCanvas.tsx
+++ src/components/ConfiguratorCanvas.tsx
@@ -46,7 +46,6 @@ const ConfiguratorCanvas = () => {
           </mesh>
         </PresentationControls>
         <Environment preset="studio" />
-        <OrbitControls/>
       </Canvas>
     </div>
   );
