d00e9081e92d72542526bead7d2ba574bbda0ea6
pranshu-kumar <pranshukumargond05@gmail.com>
2024-10-28 14:21:38 +0530

    responsive design done

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const {watchState} = useWatchContext();
 
   return (
-    <div className="h-screen bg-stone-200 flex gap-10 justify-center p-20">
+    <div className="h-screen bg-stone-200 flex gap-10 justify-center p-10">
       <div className="w-full h-full gap-2 flex flex-col">
         <h1>Apple Watch <span style={{ color: watchState['band-outside'].color }}>Ultra</span></h1>
         <h2 className="font-thin">$400</h2>
